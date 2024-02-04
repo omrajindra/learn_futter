6d62116dad1290b1e685fff575d1f2724e10fda9
Rajendra_Tharu <rajendra.075bct079@acem.edu.np>
2024-02-04 18:35:19 +0000

    1. Learn to add container

---
+++ lib/app/app.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+
+// class my_app extends StatelessWidget {
+//   const my_app({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "Raj Industry", home: Container(color: Colors.blue));
+//   }
+// }
